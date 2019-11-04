0a1c5e906061039676d1bab751f9f2c9fe5a0c48
Chris Grych <ceggrych@gmail.com>
2019-11-03 21:53:33 -0600

    Adds splash screen and credits

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Menu Canvases")]
+    [SerializeField] GameObject mainMenuCanvas;
+    [SerializeField] GameObject creditsCanvas;
+
+    [Header("Credit Objects")]
+    [SerializeField] GameObject credits1;
+    [SerializeField] GameObject credits2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenuCanvas.SetActive(true);
+        creditsCanvas.SetActive(false);
+    }
+
+    public void ViewCredits()
+    {
+        mainMenuCanvas.SetActive(false);
+        creditsCanvas.SetActive(true);
+        credits1.SetActive(true);
+        credits2.SetActive(false);
+    }
+
+    public void NextCredits()
+    {
+        credits1.SetActive(false);
+        credits2.SetActive(true);
+    }
+
+    public void PreviousCredits()
+    {
+        credits1.SetActive(true);
+        credits2.SetActive(false);
+    }
+
+    public void BackToMenu()
+    {
+        mainMenuCanvas.SetActive(true);
+        creditsCanvas.SetActive(false);
+    }
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(0); // TODO spice up?
+#endif
+    }
+
+    public void StartGame()
+    {
+        int currentSceneIndex = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+        currentSceneIndex++;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
